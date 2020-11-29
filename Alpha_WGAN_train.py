eec4eb17530b1057b7281d77929739a429be5e31
peteryuan1986 <h_yuan@uncg.edu>
2020-11-28 23:51:07 -0500

    ad

--- Alpha_WGAN_train.py
+++ Alpha_WGAN_train.py
@@ -21,7 +21,7 @@ def parse_args():
     parser.add_argument('--img_depth', type=int, default='64', help='img_depth')
     parser.add_argument('--grw', type=int, default='10', help='gradient_penalty_weight: Lamda1')
     parser.add_argument('--lamda2', type=int, default='10', help='Lamda2 in G_loss')
-    parser.add_argument('--lr', type=int, default=1e-6, help='learning rate for all four model')
+    parser.add_argument('--lr', type=int, default=1e-5, help='learning rate for all four model')
     parser.add_argument('--beta1', type=float, default=0.5, help='Decay rate for 1st moment of Adam')
     parser.add_argument('--latentdimension', type=int, default=1000, help='latent dimension')
     parser.add_argument('--iteration', type=int, default=200000, help='total iteration')
